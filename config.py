11deb8d5ef571fd455b88eec3eab99860c673946
Gunal <gunalb20@gmail.com>
2026-07-10 23:32:15 +0530

    fixed soime issues

--- config.py
+++ config.py
@@ -10,3 +10,4 @@ class Config:
     MYSQL_USER = os.getenv("MYSQL_USER")
     MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
     MYSQL_DB = os.getenv("MYSQL_DB")
+    MYSQL_PORT = int(os.getenv("MYSQL_PORT", 3306))
